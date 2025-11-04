24bfa362300a95d8b1892843a94bc26e2f8256d3
Mayank Mokta <mayankmokta@gmail.com>
2025-11-05 02:47:22 +0530

    tests updated

--- smart_contracts/test/MinimalTest.t.sol
+++ smart_contracts/test/MinimalTest.t.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.28;
 import {Test} from "forge-std/Test.sol";
 import {MinimalAccount} from "../contracts/MinimalAccount.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import { UserOperation } from "@account-abstraction/contracts/interfaces/UserOperation.sol";
 
 contract MinimalTest is Test {
 
@@ -12,10 +13,11 @@ contract MinimalTest is Test {
     address public owner;
     address public entryPoint;
     address public recipient;
+    uint256 public ownerKey;
 
     function setUp() public {
         entryPoint = makeAddr("entryPoint");
-        owner = makeAddr("owner");
+        (owner,ownerKey) = makeAddrAndKey("owner");
         recipient = makeAddr("recipient");
         vm.deal(owner,10 ether);
         
@@ -42,21 +44,96 @@ contract MinimalTest is Test {
         account.execute(recipient, 0.1 ether, "");
     }
 
+
+
+
     function testVerifySignatureFunction() public view {
         bytes memory data = "";
         uint256 nonce = account.getNonce();
 
         bytes32 msgHash = account.getMessageHash(recipient,0.2 ether,data,nonce);
-        // bytes32 ethHash = ECDSA.toEthSignedMessageHash(msgHash);
-        (uint8 v,bytes32 r, bytes32 s) = vm.sign(uint256(uint160(owner)),msgHash);
+        (uint8 v,bytes32 r, bytes32 s) = vm.sign(ownerKey,msgHash);
         bytes memory sig = abi.encodePacked(r,s,v);
         bool valid = account.verifySignature(recipient,0.2 ether,data,nonce,sig);
         // assertTrue(valid);
-        assertTrue(!valid, "Signature should be valid");
+        assertTrue(valid);
     }
+
+
+    function testExecuteWithSignatureFunction() public {
+        bytes memory data = "";
+        uint256 value = 0.2 ether;
+        uint256 nonce = account.getNonce();
+
+        bytes32 msgHash = account.getMessageHash(recipient, value, data, nonce);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerKey,msgHash);
+        bytes memory sig = abi.encodePacked(r,s,v);
+        uint256 beforeBalance = recipient.balance;
+        bytes memory result = account.executeWithSignature(recipient,value,data,nonce,sig);
+        uint256 afterBalance = recipient.balance;
+        assertEq(afterBalance - beforeBalance , value);
+    }
+
+    function testOnlyOwnerCanChangeOwner() public {
+        address attacker = makeAddr("attacker");
+        vm.prank(attacker);
+        vm.expectRevert(MinimalAccount.MA_NotOwner.selector);
+        account.changeOwner(recipient);
+    }
+
+
+     /* ========== VALIDATEUSEROP TESTS ========== */
+
+//     struct UserOperation {
+//     address sender;
+//     uint256 nonce;
+//     bytes initCode;
+//     bytes callData;
+//     uint256 callGasLimit;
+//     uint256 verificationGasLimit;
+//     uint256 preVerificationGas;
+//     uint256 maxFeePerGas;
+//     uint256 maxPriorityFeePerGas;
+//     bytes paymasterAndData;
+//     bytes signature;
+// }
+
+    // function testValidateUserOp_ValidSignature() public {
+    //     uint256 value = 0.2 ether;
+    //     UserOperation memory userOp;
+    //     userOp.sender = address(account);
+    //     userOp.callData = "";
+    //     userOp.nonce = account.getNonce();
+
+    //     bytes32 userOpHash = keccak256((abi.encode(userOp.sender,userOp.callData,userOp.nonce)));
+    //     bytes32 msgHash = ECDSA.toEthSignedMessageHash(userOpHash);
+
+    //     (uint8 v,bytes32 r,bytes32 s) = vm.sign(ownerKey,msgHash);
+    //     // userOp.signature = abi.encodePacked(r,s,v);
+    //     userOp.signature = bytes.concat(r, s, bytes1(v));
+     
+    //     vm.prank(entryPoint);
+    //     uint256 result = account.validateUserOp(userOp,msgHash,value);
+    //     assertEq(result,0);
+    // }
+
+
+ 
+
+    
+
 
 
 
     
+
+
+   
+
+
+
+    
+
+
 
 }
